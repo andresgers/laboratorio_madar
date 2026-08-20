f163f173c3ad4d345565cb5fbe79963703e36067
Andres Gerstner <agers@MacBook-Air-de-Andres.local>
2026-08-20 12:08:44 -0300

    fix: actualizar base path a /laboratorio_madar/ y regenerar build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/laboratorio_madar.github.io/',
+  base: '/laboratorio_madar/',
 })
